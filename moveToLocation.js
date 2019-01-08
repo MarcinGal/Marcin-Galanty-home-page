559fbb0cc26fe811023d4236d2840c413ccc049c
marcingal <marcin921125@gmail.com>
2019-01-08 15:53:12 +0100

    navigation-links works well

---
+++ moveToLocation.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $('#navigation-list li').click(function(){
+      var id = "#" + $(this).attr('goto');
+      var top = $(id).position().top;
+      $('html').scrollTop(top);
+    });
+  });
+
+  console.log('placuszki')
